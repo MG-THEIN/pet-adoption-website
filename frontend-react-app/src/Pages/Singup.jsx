8b6c88804ed5d0183adcbf4ac63a420b7a822fb8
MG-THEIN <mt4130c@gmail.com>
2024-03-23 14:54:47 -0400

    connected singup form to the api

--- frontend-react-app/src/Pages/Singup.jsx
+++ frontend-react-app/src/Pages/Singup.jsx
@@ -1,27 +1,67 @@
-import React from "react"
+import React, { useState } from "react"
 import "./CSS/Signup.css"
 import { Link } from "react-router-dom"
 
 const Singup = () => {
+  const [formData, setFormData] = useState({})
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    })
+  }
+
+  console.log(formData)
+
   return (
-    <div className="signup">
+    <form className="signup">
       <div className="signup__header">
         <p>Sign Up</p>
       </div>
       <div className="signup__container">
-        <input type="text" placeholder="First Name" />
-        <input type="text" placeholder="Last Name" />
-        <select required>
-          <option value="" disabled selected>
+        <input
+          type="text"
+          placeholder="First Name"
+          id="first_name"
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="Last Name"
+          id="last_name"
+          onChange={handleChange}
+        />
+        <select
+          required
+          defaultValue=""
+          id="country_of_residence"
+          onChange={handleChange}>
+          <option value="" disabled>
             Country of Residence
           </option>
           <option>United State</option>
           <option>Mexico</option>
           <option>Canada</option>
         </select>
-        <input type="text" placeholder="ZIP Code" />
-        <input type="text" placeholder="Email" />
-        <input type="text" placeholder="Password" />
+        <input
+          type="text"
+          placeholder="ZIP Code"
+          id="zip_code"
+          onChange={handleChange}
+        />
+        <input
+          type="email"
+          placeholder="Email"
+          id="email"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          id="password"
+          onChange={handleChange}
+        />
         <div className="signup__container--user-agreement">
           <input type="checkbox" name="" id="" />
           <p>
@@ -39,7 +79,7 @@ const Singup = () => {
           </Link>
         </p>
       </div>
-    </div>
+    </form>
   )
 }
 
